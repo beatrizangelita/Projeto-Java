03875e956d460f15e4b5b22a7ca45dbd00aa0d3c
Adriane Felicio <adrianefelicios@outlook.com>
2023-03-27 20:18:23 -0300

    esboco

---
+++ esboço-projeto/Testeprojetojava.java
@@ -0,0 +1,54 @@
+package Exercicios;
+
+public class Testeprojetojava {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("                             _________________________________________________________________________");
+		System.out.println("                            |                            RELATÓRIO GERAL                              |");
+		System.out.println("                            |_________________________________________________________________________|");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |     					         GASTOS                                   |");
+		System.out.println("                            |    Total Despesas Fixas:                                                |");
+		System.out.println("                            |    if(totaldesp < 30% ){                                                |");
+		System.out.println("                            |           'O Tio Patinhas está orgulhoso de você!                       |");
+		System.out.println("                            |             onseguiu manter suas despesas dentro do esperado!'          |");        
+		System.out.println("                            |    } else {                                                             |");
+		System.out.println("                            |    'O tio Patinhas está desapontado! Você ultrapassou X% do orçamento.  |");
+		System.out.println("                            |      Recomendamos que releia as preciosas dicas do Tio Patinhas!        |");
+		System.out.println("                            |_________________________________________________________________________|");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |    Total Despesas Variáveis:                                            |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |    if(totaldesp < 30% ){                                                |");
+		System.out.println("                            |           'O Tio Patinhas está orgulhoso de você!                       |");
+		System.out.println("                            |             onseguiu manter suas despesas dentro do esperado!'          |");        
+		System.out.println("                            |    } else {                                                             |");
+		System.out.println("                            |    'O tio Patinhas está desapontado! Você ultrapassou X% do orçamento.  |");
+		System.out.println("                            |      Recomendamos que releia as preciosas dicas do Tio Patinhas!        |");
+		System.out.println("                            |_________________________________________________________________________|");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |   Total de Investimentos:                                               |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |_________________________________________________________________________|");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |   Total de Poupança:                                                    |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |_________________________________________________________________________|");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |  Total de Renda Restante:                                               |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |                                                                         |");
+		System.out.println("                            |_________________________________________________________________________|");
+
+	
+	
+	}
+
+}
